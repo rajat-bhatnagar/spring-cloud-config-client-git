e418452731e74fdd50b6f89293028b489428329d
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-01-22 23:36:41 -0500

    Initial Commit

---
+++ src/main/java/com/coding/thoughtworks/config/client/SpringCloudConfigClientGitApplication.java
@@ -0,0 +1,12 @@
+package com.coding.thoughtworks.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientGitApplication.class, args);
+	}
+}
